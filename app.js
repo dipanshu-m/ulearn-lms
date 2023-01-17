69ad6e7f8a730716ecc2bd2f94a58965b9773885
Dipanshu Mahato <dipanshumahato@gmail.com>
2023-01-17 13:22:06 +0530

    FIX: Application crash upon updating dashboard

--- app.js
+++ app.js
@@ -17,44 +17,49 @@ app.use(express.static('./uploads/'));
 // connect to database
 username = process.env.DB_UNAME;
 password = process.env.DB_PASS;
-const dbURI = `mongodb+srv://${username}:${password}@cluster0.obtv8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        console.log("connected to db");
-        const port = process.env.PORT || 5000;
-        app.listen(port, () => {
-            console.log(`Listening on http://localhost:${port}/`);
-        });
-    })
-    .catch((err) => {
-        console.log(err);
-    })
+const dbURI = `mongodb+srv://admin:qwerty1234567890@cluster0.wnudgdi.mongodb.net/?retryWrites=true&w=majority`;
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 5000,
+  autoIndex: false, // Don't build indexes
+  maxPoolSize: 10, // Maintain up to 10 socket connections
+  socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+  family: 4, // Use IPv4, skip trying IPv6
+  dbName: 'Users',
+};
+mongoose.set('strictQuery', true);
+mongoose.connect(dbURI, options ).then(() => {
+  console.log('connected to db');
+  const port = process.env.PORT || 5000;
+  app.listen(port, () => {
+    console.log(`Listening on http://localhost:${port}/`);
+  });
+});
 //routes
 app.get('*', checkUser);
 app.get('/', (req, res) => {
-    console.log("homepage");
-    res.sendFile(path.resolve(__dirname, './views/index.html'))
-
+  console.log('homepage');
+  res.sendFile(path.resolve(__dirname, './views/index.html'));
 });
 app.use(authRoutes);
-app.set("view engine", "ejs");
-app.use('/task',taskRoutes);
+app.set('view engine', 'ejs');
+app.use('/task', taskRoutes);
 
 app.get('/dashboard', requireAuth, (req, res) => {
-
-    // console.log("id="+res.locals.user._id);
-    res.render('./tasks/dashboard.ejs', {
-        image: res.locals.user.image,
-        fullname: res.locals.user.fullname,
-        username: res.locals.user.username,
-        email: res.locals.user.image,
-        password: res.locals.user.image
-    })
-})
+  // console.log("id="+res.locals.user._id);
+  res.render('./tasks/dashboard.ejs', {
+    image: res.locals.user.image,
+    fullname: res.locals.user.fullname,
+    username: res.locals.user.username,
+    email: res.locals.user.image,
+    password: res.locals.user.image,
+  });
+});
 
 // app.get('userinfo', requireAuth, (req, res)=> {
 //     res.json()
 // })
 app.get('/tasks', requireAuth, (req, res) => {
-    res.render('./tasks/tasks')
+  res.render('./tasks/tasks');
 });
